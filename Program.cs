6435c91854f84181b82a58b8b4e938556ee682d9
DevAniela <dismal.damsel@gmail.com>
2026-02-07 09:49:07 +0200

    feat: implement global exception handling middleware

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using WorkshopBooking.Data;
+using WorkshopBooking.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
