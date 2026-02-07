40ec99b0a3a4af3d96f3ad6a4d0db58bbce1f3ba
DevAniela <dismal.damsel@gmail.com>
2026-02-07 11:05:11 +0200

    feat: add CORS policy and basic HTML/JS frontend

--- Program.cs
+++ Program.cs
@@ -11,6 +11,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
@@ -21,8 +29,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
+
 app.MapControllers();
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.Run();
